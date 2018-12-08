eb18aaa1635d9c042e5715d225200b1e9fa576fa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-08 13:03:25 +0000

    Deploy Jensen95/TypeSearch to github.com/Jensen95/TypeSearch.git:gh-pages

--- script/search.js
+++ script/search.js
@@ -1,4 +1,4 @@
-var searchIndexUrl = "https://typespublisher.blob.core.windows.net/typespublisher/data/search-index-min.json";
+var searchIndexUrl = 'https://typespublisher.blob.core.windows.net/typespublisher/data/search-index-min.json';
 function typeSearch(el) {
     var jqueryEl = $(el);
     var opts = {
@@ -18,15 +18,16 @@ function typeSearch(el) {
     jqueryEl.on('typeahead:select', function (unused, obj) { return navigate(obj); });
     jqueryEl.keyup(function (k) {
         if (k.keyCode === 13) {
-            var selectables = jqueryEl.siblings(".tt-menu").find(".tt-selectable");
-            $(selectables[0]).trigger("click");
+            // Enter key
+            var selectables = jqueryEl.siblings('.tt-menu').find('.tt-selectable');
+            $(selectables[0]).trigger('click');
         }
     });
     function navigate(record) {
-        window.location.href = "https://www.npmjs.org/package/@types/" + record.t;
+        window.open("https://yarnpkg.com/en/package/@types/" + record.t, '_blank');
     }
     function createDataSource() {
-        var query = "";
+        var query = '';
         return new Bloodhound({
             // See https://github.com/twitter/typeahead.js/blob/master/doc/bloodhound.md#options
             prefetch: searchIndexUrl,
@@ -41,10 +42,10 @@ function typeSearch(el) {
             sorter: function (x, y) {
                 // TODO: Include edit distance as additional weighting factor
                 // Direct matches should be ranked higher, else rank on basis of download count
-                if (x.t === query || x.t === (query + "js") || x.t === (query + ".js") || x.t === (query + "-js")) {
+                if (x.t === query || x.t === query + 'js' || x.t === query + '.js' || x.t === query + '-js') {
                     return -1;
                 }
-                else if (y.t === query || y.t === (query + "js") || y.t === (query + ".js") || y.t === (query + "-js")) {
+                else if (y.t === query || y.t === query + 'js' || y.t === query + '.js' || y.t === query + '-js') {
                     return 1;
                 }
                 else {
